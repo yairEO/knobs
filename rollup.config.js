b8e1ebb27d18d035c032cbdeff27dc1cd3084761
vsync <vsync.design@gmail.com>
2020-12-20 16:24:21 +0200

    [chore] - refactored rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,44 +1,34 @@
-import scss from 'rollup-plugin-scss'
-import babel from '@rollup/plugin-babel'
-import { terser } from 'rollup-plugin-terser'
-import cleanup from 'rollup-plugin-cleanup'
-import serve from 'rollup-plugin-serve'
-import livereload from 'rollup-plugin-livereload'
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+import scss            from 'rollup-plugin-scss'
+import babel           from '@rollup/plugin-babel'
+import { terser }      from 'rollup-plugin-terser'
+import cleanup         from 'rollup-plugin-cleanup'
+import serve           from 'rollup-plugin-serve'
+import livereload      from 'rollup-plugin-livereload'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs        from '@rollup/plugin-commonjs'
+import pkg             from './package.json'
+
+const banner = `/*! Knobs ${pkg.version} MIT | https://github.com/yairEO/knobs */\n`;
 
 export default [
   {
     input: 'src/knobs.js',
     output: {
+      banner,
       file: 'knobs.min.js',
       format: 'umd',
       name: 'Knobs',
     },
     plugins: [
-      terser(),
+      serve(), // index.html should be in root of project
+      livereload({ watch:'src', delay:1500, exts: [ 'html', 'js', 'scss', 'css' ] }),
+    //  terser(),
       babel({ babelHelpers: 'bundled' }),
       scss({ output: false, outputStyle: 'compressed', watch: 'src/styles', }),
       cleanup(),
       nodeResolve(),
       commonjs()
     ]
-  },
-  {
-    input: 'src/knobs.js',
-    output: {
-      file: 'knobs.js',
-      format: 'umd',
-      name: 'Knobs',
-    },
-    plugins: [
-      serve(), // index.html should be in root of project
-      livereload({ watch:'src', delay:1000, exts: [ 'html', 'js', 'scss', 'css' ] }),
-      scss({ output: false, outputStyle: 'compressed', watch: 'src/styles', }),
-      cleanup(),
-      nodeResolve(),
-      commonjs()
-    ]
-  },
+  }
 ]
 
